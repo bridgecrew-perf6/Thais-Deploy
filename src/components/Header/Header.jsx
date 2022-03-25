6e04f38ccdd04b054240dfd2c7410216d76bc182
zailua <84216764+zailua@users.noreply.github.com>
2022-03-24 21:12:55 -0300

    up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,36 +11,56 @@ import './Header.css'
 
 function Header() {
 	return (
-		
-		
-	<header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="../src/assets/icone.png" alt="" width="35" height="50" class="d-inline-block align-text-center"/>
-     Viva! Viagens, a sua agência de viagens favorita.
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse " id="navbarNav">
-      <ul class="navbar-nav align-text-right">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Home <span class="glyphicon glyphicon-home"></span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/Destinos">Destinos  <span class="
-glyphicon glyphicon-map-marker"></span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/Promocoes">Promoções <span class="glyphicon glyphicon-gift"></span></a>
-        </li>
-      
-      </ul>
-    </div>
-  </div>
-</nav>
-	</header>
+		<header>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						<img
+							src="../src/assets/icone.png"
+							alt=""
+							width="35"
+							height="50"
+							className="d-inline-block align-text-center"
+						/>
+						Viva! Viagens, a sua agência de viagens favorita.
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse " id="navbarNav">
+						<ul className="navbar-nav align-text-right">
+							<li className="nav-item">
+								<NavLink className="nav-link" aria-current="page" to="/">
+									Home <span className="glyphicon glyphicon-home"></span>
+								</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink className="nav-link" to="/Destinos">
+									Destinos
+									<span
+										className="
+                    glyphicon glyph-icon-airplane"
+									></span>
+								</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink className="nav-link" to="/Promocoes">
+									Promoções <span className="glyphicon glyphicon-gift"></span>
+								</NavLink>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
 	)
 }
 export default Header
